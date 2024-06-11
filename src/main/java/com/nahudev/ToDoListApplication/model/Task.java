1c41f22d6278ef83d7920c6c8cadb3ce3083d88f
nahuel-19 <nahueldelatorre16@gmail.com>
2024-06-11 18:42:36 -0300

    Estructura del proyecto

---
+++ src/main/java/com/nahudev/ToDoListApplication/model/Task.java
@@ -0,0 +1,65 @@
+package com.nahudev.ToDoListApplication.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ValueGenerationType;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Tareas")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_task")
+    private Long id_task;
+
+    @Column(name = "titulo")
+    private String title;
+
+    @Column(name = "descripcion")
+    private String description;
+
+    @Column(name = "fecha_creacion")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private LocalDate creationDate;
+
+    @Column(name = "estado")
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    @JsonBackReference("id_user")
+    private User user;
+
+    public Task() {
+    }
+
+    public Task(Long id_task, String title, String description,
+                LocalDate creationDate, String state, User user) {
+        this.id_task = id_task;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+        this.state = state;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id_task=" + id_task +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", creationDate=" + creationDate +
+                ", state='" + state + '\'' +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ src/main/java/com/nahudev/ToDoListApplication/model/User.java
@@ -0,0 +1,62 @@
+package com.nahudev.ToDoListApplication.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Usuarios")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id_user;
+
+    @Column(name = "nombre")
+    private String name;
+
+    @Column(name = "apellido")
+    private String lastName;
+
+    @Column(name = "fecha_nacimiento")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    private LocalDate birthdate;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
+    private List<Task> listTasks;
+
+    public User() {
+    }
+
+    public User(Long id_user, String name, String lastName,
+                LocalDate birthdate, String email, List<Task> listTasks) {
+        this.id_user = id_user;
+        this.name = name;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.listTasks = listTasks;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id_user=" + id_user +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthdate +
+                ", email='" + email + '\'' +
+                ", listTasks=" + listTasks +
+                '}';
+    }
+}
